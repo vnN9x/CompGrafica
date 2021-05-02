b0c019fa4c767627d621677bc2cb60b2931c99b4
gartunius <gabriel.ataidesantos@gmail.com>
2021-05-02 18:25:11 -0300

    Falta checagem nos imports de packages

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -1,11 +1,12 @@
 package teste;
 
+import entity.Quadrado;
 import view.Janela;
 import view.Plano;
 
 public class Teste {
 	public static void main(String[] args) {
-		new Janela(new Plano());
+		new Janela(new Plano(new Quadrado()));
 		
 		
 		
--- src/view/Plano.java
+++ src/view/Plano.java
@@ -1,5 +1,6 @@
 package view;
 
+import entity.Quadrado;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -9,10 +10,44 @@ import javax.swing.JPanel;
 
 public class Plano extends JPanel {
 
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D)g;
-		this.setBackground(Color.BLACK);
-	}
-}
+    Quadrado forma;
+
+    public Plano(Quadrado forma) {
+        this.forma = forma;
+    }
+
+    public void desenhoUpdate(Quadrado forma) {
+        this.forma = forma;
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D)g;
+
+        this.setBackground(new Color(43, 43, 43));
+
+        g2d.setStroke(new BasicStroke(15));
+
+        List<Ponto> pontos = desenho.getPontos(); 
+
+        if(pontos==null) {
+            System.out.println("Desenho eh nulo");
+            return;
+        }
+
+        Ponto pontoAnterior = pontos.get(pontos.size()-1);
+        Cores cor = Cores.values()[0];
+        for(Ponto pontoAtual: pontos) {
+            g2d.setColor(cor.getCor());
+            g2d.drawLine(
+                    (int)pontoAnterior.x, (int)pontoAnterior.y, 
+                    (int)pontoAtual.x,    (int)pontoAtual.y);
+
+            cor = cor.next();
+            pontoAnterior = pontoAtual;
+
+        }
+    }
+
